5bfca0dced3b139f9e84752978646be8442f5696
ileeli <elert.bartosz@gmail.com>
2022-03-22 17:08:11 +0100

    added missing tablet+ styles for larger screens

--- src/components/App/Header.jsx
+++ src/components/App/Header.jsx
@@ -5,8 +5,9 @@ import PropTypes from 'prop-types';
 function Header({ isDarkModeToggled, onDarkModeToggleClick }) {
     return (
         <header
-            className="relative py-0.5 mb-12 border border-gray-700
-            rounded-lg bg-zinc-50 dark:border-zinc-50 dark:bg-gray-700">
+            className="w-full relative py-0.5 mb-12 border border-gray-700
+            rounded-lg bg-zinc-50 dark:border-zinc-50 dark:bg-gray-700
+            sm:w-3/5 sm:min-w-320 lg:w-2/5">
             <h1 className="font-semibold text-center text-2xl dark:text-zinc-50">Doify</h1>
             <button
                 className="absolute right-0 top-1/2 transform -translate-y-1/2 h-full bg-gray-700
